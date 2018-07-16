13d9498e75baea2afecb04b96f04ee07bd0891aa
ayushsingh31 <927ayush8@gmail.com>
2018-07-16 15:27:06 +0530

    changes made

--- bot.js
+++ bot.js
@@ -21,11 +21,11 @@ function followed(eventMsg){
 
   T.post('friendships/create', { screen_name: screenName}, function(err, data, response)
     {
-      tweetIt('@' + screenName+ ' Thanks for following me');
+      tweetIt('@' + screenName+ ' Thanks for following me, see i followed you back');
       console.log("you are following the user");
     });
 }
-
+stream.stop();
 // function tweetEvent(eventMsg){
 //   //var replyto =  eventMsg.source.screen_name;
 //  var replyto = eventMsg.in_reply_to_screen_name;
