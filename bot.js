a6bd29150c248872d6b5131fed362373eed6d0e5
ayushsingh31 <927ayush8@gmail.com>
2018-07-16 13:43:28 +0530

    My first twitter bot

---
+++ bot.js
@@ -0,0 +1,50 @@
+var Twit = require('twit');
+var config = require('./config');
+var T = new Twit(config);
+
+var stream = T.stream('user');
+stream.on('tweet',tweetEvent);
+stream.on('follow',followed);
+// stream.on('followback',follow);
+
+// function followback(){
+//   var name= eventMsg.source.name;
+//   var screenName = eventMsg.source.screen_name;
+//
+// }
+
+function followed(eventMsg){
+  var name= eventMsg.source.name;
+  var screenName = eventMsg.source.screen_name;
+  var id =eventMsg.source.id;
+  T.post('friendships/create', { screen_name: screenName}, function(err, data, response)
+    {
+      tweetIt('@' + screenName+ ' Thanks for following me');
+
+      console.log("you are following the user");
+    });
+}
+
+function tweetEvent(eventMsg){
+var replyto = eventMsg.in_reply_to_screen_name;
+  var text =eventMsg.text;
+  var from = eventMsg.user.screen_name;
+
+  if(replyto==='ayush____singh'){
+var newTweet = '@' + from + ' thank you for tweeting me';
+tweetIt(newTweet);
+console.log("you are following the user");
+
+  }
+}
+
+function tweetIt(txt){
+
+var tweet = {
+  status:txt
+}
+T.post('statuses/update', tweet, function(err, data, response) {
+  console.log(data);
+});
+
+}
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  consumer_key:         'uH7EaP6WPSqi8TE4aSyNGCnWw',
+  consumer_secret:      'S8gRtIyiucgqJBVhMi1JYL0i6YM3rgHhDJq50E2En3FKbJ506t',
+  access_token:         '953966242037080064-vUO5m9pPCXmQQ3F9toaTiyjoNymL5po',
+  access_token_secret:  'cCTFbQuLxv3YICzsgLIrcu8tepHjJzdNCGNGduavaYcjx'
+
+
+
+};
